72ad82c1a567c2fd3aebb2159f0ff765b0173bb1
MSuhail <owais0203@gmail.com>
2024-07-15 10:36:17 +0200

    Adding to Github the Second microservice

---
+++ src/main/java/com/example/jobms/job/Impl/JobServiceImpl.java
@@ -0,0 +1,128 @@
+package com.example.jobms.job.Impl;
+
+import com.example.jobms.job.Job;
+import com.example.jobms.job.JobRepository;
+import com.example.jobms.job.JobService;
+import com.example.jobms.job.clients.CompanyClient;
+import com.example.jobms.job.clients.ReviewClient;
+import com.example.jobms.job.dto.JobDTO;
+import com.example.jobms.job.external.Company;
+import com.example.jobms.job.external.Review;
+import com.example.jobms.job.mapper.JobMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class JobServiceImpl implements JobService {
+
+    // private List<Job> jobs = new ArrayList<>();
+    JobRepository jobRepository;
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    private final CompanyClient companyClient;
+    private final ReviewClient reviewClient;
+
+    public JobServiceImpl(JobRepository jobRepository, CompanyClient companyClient, ReviewClient reviewClient) {
+        this.jobRepository = jobRepository;
+        this.companyClient = companyClient;
+        this.reviewClient = reviewClient;
+    }
+
+    private Long nextId = 1L;
+
+    @Override
+    public List<JobDTO> findAll() {
+
+        List<Job> jobs = jobRepository.findAll();
+        // List<JobDTO> jobDTOS = new ArrayList<>();
+
+        // RestTemplate restTemplate = new RestTemplate();
+
+//        for (Job job : jobs) {
+//            JobWithCompanyDTO jobWithCompanyDTO = new JobWithCompanyDTO();
+//            jobWithCompanyDTO.setJob(job);
+//
+//            Company company = restTemplate.getForObject("http://COMPANY-SERVICE:8081/companies/" + job.getCompanyId(), Company.class);
+//
+//            jobWithCompanyDTO.setCompany(company);
+//
+//            jobWithCompanyDTOs.add(jobWithCompanyDTO);
+//        }
+//
+//        return jobWithCompanyDTOs;
+
+        return jobs.stream().map(this::convertToDto).collect(Collectors.toList());
+    }
+
+    private JobDTO convertToDto(Job job) {
+        // JobWithCompanyDTO jobWithCompanyDTO = new JobWithCompanyDTO();
+        // jobWithCompanyDTO.setJob(job);
+
+        // Useful when you know that the response type is a single object
+        // Company company = restTemplate.getForObject("http://COMPANY-SERVICE:8081/companies/" + job.getCompanyId(), Company.class);
+        Company company = companyClient.getCompany(job.getCompanyId());
+
+        // Exchange is useful/versatile when response type is a generic collections
+//        ResponseEntity<List<Review>> reviewResponse = restTemplate.exchange("http://REVIEW-SERVICE:8083/reviews?companyId=" + job.getCompanyId(), HttpMethod.GET, null, new ParameterizedTypeReference<List<Review>>() {
+//        });
+        List<Review> reviews = reviewClient.getReviews(job.getCompanyId());
+
+
+        // List<Review> reviews = reviewResponse.getBody();
+
+        JobDTO jobDTO = JobMapper.mapToJobWithCompanyDTO(job, company, reviews);
+
+        return jobDTO;
+    }
+
+    @Override
+    public void createJob(Job job) {
+        job.setId(nextId++);
+        jobRepository.save(job);
+    }
+
+    @Override
+    public JobDTO getJobById(Long id) {
+        Job job = jobRepository.findById(id).orElseThrow(null);
+        return convertToDto(job);
+    }
+
+    @Override
+    public boolean deleteJobById(Long id) {
+        try {
+            jobRepository.deleteById(id);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean updateJob(Long id, Job updatedJob) {
+        Optional<Job> jobOptional = jobRepository.findById(id);
+        if (jobOptional.isPresent()) {
+            Job job = jobOptional.get();
+            job.setTitle(updatedJob.getTitle());
+            job.setDescription(updatedJob.getDescription());
+            job.setMinSalary(updatedJob.getMinSalary());
+            job.setMaxSalary(updatedJob.getMaxSalary());
+            job.setLocation(updatedJob.getLocation());
+            jobRepository.save(job);
+            return true;
+        }
+        return false;
+    }
+
+
+}
